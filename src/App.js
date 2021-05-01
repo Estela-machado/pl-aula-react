19351f427d9de7c35d20dd00a55a0cc8bccf48f7
Adrison Luz <adrison701@gmail.com>
2021-05-01 17:05:02 -0300

    Aula 3

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './react.png';
+import './App.css';
+import BlocoHome from './componentes/bloco-home';
+
+function App() {
+  return (
+    <BlocoHome logo={logo}/>
+  );
+}
+
+export default App;
